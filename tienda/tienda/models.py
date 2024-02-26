e97a8d8466462b4b27a8ad74f550be471c03a703
spawnew <96084099+spawnew@users.noreply.github.com>
2024-02-26 19:33:22 -0300

    empezando la pagina

---
+++ tienda/tienda/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Producto (models.Model):
+    
+        nombre = models.CharField(max_length=40)
+        precio = models.IntegerField()
+
+        def __str__(self):
+             return f"{self.nombre}"
+    
